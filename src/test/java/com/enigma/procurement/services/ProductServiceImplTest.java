ab2fd023459b6a397dc4193e374970301d2b17d5
Pramudya Diva Alamsyah <pramudyaalamsyah@gmail.com>
2022-12-09 22:28:07 +0700

    CLEAR TESTING FOR PRODUCT

--- src/test/java/com/enigma/procurement/services/ProductServiceImplTest.java
+++ src/test/java/com/enigma/procurement/services/ProductServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.enigma.procurement.services;
 
+import com.enigma.procurement.exception.NotFoundException;
 import com.enigma.procurement.models.Category;
 import com.enigma.procurement.models.Product;
 import com.enigma.procurement.repositories.CategoryRepository;
@@ -15,7 +16,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -47,6 +50,46 @@ class ProductServiceImplTest {
         dummyProduct.setProductId("1");
         dummyProduct.setProductName("product 1");
 
+        when(mockCategoryRepository.findById(anyString())).thenReturn(Optional.of(dummyCategory));
+
+        dummyProduct.setCategory(dummyCategory);
+
+        when(mockProductRepository.save(any())).thenReturn(dummyProduct);
+
+        Product actualProduct = productService.create(dummyProduct);
+        Assertions.assertEquals(dummyProduct.getProductId(),actualProduct.getProductId());
+    }
+
+
+    @Test
+    void itShould_ThrowException_When_CategoryFindIdError() throws Exception{
+        Category dummyCategory = new Category();
+
+        dummyCategory.setCategoryId("1");
+        dummyCategory.setCategoryName("BAJU ANAK ANAK");
+
+        Product dummyProduct = new Product();
+
+        dummyProduct.setProductId("1");
+        dummyProduct.setProductName("product 1");
+
+        dummyProduct.setCategory(dummyCategory);
+
+        when(mockCategoryRepository.findById(anyString())).thenThrow(NullPointerException.class);
+        Assertions.assertThrows(RuntimeException.class,() -> productService.create(dummyProduct));
+    }
+
+    @Test
+    void itShould_ReturnListProduct_When_ProductServiceGetAllProduct() throws Exception{
+        Category dummyCategory = new Category();
+
+        dummyCategory.setCategoryId("1");
+        dummyCategory.setCategoryName("BAJU ANAK ANAK");
+
+        Product dummyProduct = new Product();
+
+        dummyProduct.setProductId("1");
+        dummyProduct.setProductName("product 1");
 
         dummyProduct.setCategory(dummyCategory);
 
@@ -57,6 +100,12 @@ class ProductServiceImplTest {
         List<Product> actualProduct = productService.getAll();
 
         Assertions.assertEquals(1, actualProduct.size());
+    }
+
+    @Test
+    void itShould_ThrowException_When_ProductServiceListGetError() throws Exception{
+        when(mockProductRepository.findAll()).thenThrow(NullPointerException.class);
+        Assertions.assertThrows(RuntimeException.class,() -> productService.getAll());
     }
 
 }
